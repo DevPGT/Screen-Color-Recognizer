780843d5b7bba116b77ee8bcf69ba44fe523ad6c
David Reis <d.n.reis2014@gmail.com>
2019-11-09 02:26:28 -0300

    Transforming in a non-static code

--- Screen.java
+++ Screen.java
@@ -6,64 +6,68 @@ import java.awt.MouseInfo;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
 
-public class Green {
+public class Screen {
 	
-	static Robot robot;
+	private Robot robot;
 	
-	static int[] x , y;
-	static int size = 0;
-	static Color Cor;
-	static int MRGB[];
-	static int L_TIME , R , G , B;
-	static String COLOR_NAME;
+	private int[] x , y;
+	private int size;
+	private Color cor;
+	private int MRGB[];
+	private int L_TIME , R , G , B;
+	private String COLOR_NAME;
 		
-	public Green() {
-		// TODO Auto-generated constructor stub
+	public Screen() {
+		robot = new Robot();
+		
+		MRGB = new int[]{0, 0, 0};
+		
+		y = new int[512];
+		x = new int[512];
+		
+		size = 0;
 	}
 	
 	public static void main(String[] args) throws InterruptedException, AWTException {
-		robot = new Robot();
-		MRGB = new int[]{ 0 , 0 , 0 };
-		getPixels();
+		Screen screen = new Screen();
+		screen.getPixels();
 		//wait(1000);
 		while(true) {
-			showColors();
+			screen.showColors();
 			//wait(3000);
 		}
 	}
 	
-	public static void getPixels() throws InterruptedException, AWTException {
-		y = new int[512];
-		x = new int[512];
+	public void getPixels() throws InterruptedException, AWTException {
 		print("Posicione o mouse sobre o meio da nota VERDE");
 		wait(1300);
 		//varredura();
 		drawSquare();
 	}
 	
-	public static void varredura() throws AWTException, InterruptedException {
+	public void varredura() throws AWTException, InterruptedException {
 		for(int v = 0; v < 30; v++) {
 			setMouseY(1);
 			y[v] = getPixelCord('y');
-			print("Y [" + v + "] =" + y[v]);
+			print("Y ["+v+"] = "+y[v]);
 		}
 		wait(1000);
 		setMouseY(-15);
 		for(int v = 0; v < 35; v++) {
 			setMouseX(1);
 			x[v] = getPixelCord('x');
-			print("X [" + v + "] =" + x[v]);
+			print("X ["+v+"] = "+x[v]);
 		}
 		wait(1000);
 		setMouseX(-35);
-		for(int v = 0; v < 35; v++) {
+		for(int v = 36; v < 71; v++) {
 			setMouseX(-1);
-			x[36 + v] = getPixelCord('x');
-			print("X [" + (36 + v) + "] =" + x[36 + v]);
+			x[v] = getPixelCord('x');
+			print("X ["+v+"] = "+x[v]);
 		}
 	}
 	
-	public static void drawSquare() throws AWTException, InterruptedException {
+	public void drawSquare() throws AWTException, InterruptedException {
 		int x1 = getPixelCord('x');
 		int x2 = x1 - 80;
 		int y1 = getPixelCord('y');
@@ -71,7 +75,7 @@ public class Green {
 		for(int H = x2; H < x1; H += 10){
 			for(int V = y2; V < y1; V += 10) {
 				int cor = robot.getPixelColor(H, V).getGreen();
-				if(!(cor < 20 || cor == 74 )) {
+				if(!(cor < 20 || cor == 74)) {
 					robot.mouseMove(H, V);
 					x[size] = H;
 					y[size] = V;
@@ -81,25 +85,25 @@ public class Green {
 		}
 	}
 	
-	public static void showColors() throws AWTException {
+	public void showColors() throws AWTException {
 		L_TIME = 0;
 		for(int i = 0; i < x.length; i++) {
 			if(x[i] == 0) {
 				break;
 			}
 			//robot.mouseMove(x[i], y[i]);
-			Cor = robot.getPixelColor(x[i] , y[i]);
+			cor = robot.getPixelColor(x[i] , y[i]);
 			//print("COR NA CORD [ X:" + x[i] + " ; Y:" + y[i] + " ] = " + getColor(x[i] , y[i]) );
-			MRGB[0] += Cor.getRed();
-			MRGB[1] += Cor.getGreen();
-			MRGB[2] += Cor.getBlue();
+			MRGB[0] += cor.getRed();
+			MRGB[1] += cor.getGreen();
+			MRGB[2] += cor.getBlue();
 			L_TIME++;
 		}
 		R = MRGB[0] / L_TIME;
 		G = MRGB[1] / L_TIME;
 		B = MRGB[2] / L_TIME;
-		COLOR_NAME = getColorName( R , G , B );
-		print("[" + R + " ; " + G + " ; " + B + " = " + COLOR_NAME);
+		COLOR_NAME = getColorName(R, G, B);
+		print("["+R+"; "+G+"; "+B+" = "+COLOR_NAME);
 		/*
 		if(COLOR_NAME != "") {
 			print("( " + L_TIME + ") + { " + MRGB[0] + " ; " + MRGB[1] + " ; " + MRGB[2] + " } [" + R + " ; " + G + " ; " + B + " = " + COLOR_NAME);
@@ -111,7 +115,7 @@ public class Green {
 		}
 	}
 	
-	public static String getColorName(int r , int g , int b) {
+	public String getColorName(int r , int g , int b) {
 		if(r > 200 && g < 50 && b < 50){ return "VERMELHO";}
 		if(r < 50 && g > 140 && b < 50){ return "VERDE";}
 		if(r > 150 && g > 140 && b < 50){ return "AMARELO";}
@@ -121,31 +125,31 @@ public class Green {
 		return "";
 	}
 	
-	public static Color getColor( int x , int y) throws AWTException {
+	public Color getColor(int x, int y) throws AWTException {
 		return robot.getPixelColor(x, y);
 	}
 	
-	public static void setMouseY(int pixels) throws AWTException {
-		robot.mouseMove(getMouseX(), ( getMouseY() + pixels));
+	public void setMouseY(int pixels) throws AWTException {
+		robot.mouseMove(getMouseX(), (getMouseY()+pixels));
 	}
 		
-	public static void setMouseX(int pixels) throws AWTException {
-		robot.mouseMove((getMouseX() + pixels) , getMouseY());
+	public void setMouseX(int pixels) throws AWTException {
+		robot.mouseMove((getMouseX()+pixels), getMouseY());
 	}
 	
-	public static int getMouseX() {
+	public int getMouseX() {
 		return MouseInfo.getPointerInfo().getLocation().x;
 	}
 	
-	public static int getMouseY() {
+	public int getMouseY() {
 		return MouseInfo.getPointerInfo().getLocation().y;
 	}
 	
-	public static void print(String text) {
+	public void print(String text) {
 		System.out.println(text);
 	}
 		
-	public static int getPixelCord(char p) {
+	public int getPixelCord(char p) {
 		if (p == 'x') {
 			return MouseInfo.getPointerInfo().getLocation().x;
 		}
